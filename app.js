d149e24d5e9966d28beb94f1b01b0c4bdf17c018
rkhayat <robertrkhayat@gmail.com>
2019-07-01 18:17:40 -0400

    add cors

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ const schema = new GraphQLSchema({
 
 var app = express();
 app.use( cors() );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 app.get('/test', (req, res) => res.send('Hello World!'))
 
 app.use(
