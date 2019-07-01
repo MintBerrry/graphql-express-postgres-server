ce87616a87c680744f0dd0f9f1861d0042b2f9bb
rkhayat <robertrkhayat@gmail.com>
2019-07-01 17:01:01 -0400

    add heroku connection

--- pgAdaptor.js
+++ pgAdaptor.js
@@ -1,16 +1,19 @@
-require('dotenv').config()
-const pgPromise = require('pg-promise');
+require("dotenv").config();
+const pgPromise = require("pg-promise");
 
 const pgp = pgPromise({});
-
-const config = {
+let config;
+if (process.env.NODE_ENV !== "production") {
+  config = {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD
-};
-
+  };
+} else {
+  config = process.env.DATABASE_URL;
+}
 const db = pgp(config);
 
 exports.db = db;
